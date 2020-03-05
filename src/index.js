f8de9c378b6254acfa4db91283dab4e858813d80
TimaLebed <timalebedevskiy@mail.ru>
2020-03-05 16:26:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,43 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(str) {
+    let value = '';
+    let result = '';
+    let sum = 0;
+    let isSpace = false;
+
+    for (let i = 0; i <= str.length; i = i + 2){
+        if (sum == 10 ) {
+            if (isSpace == false) {
+                value += '' + MORSE_TABLE[result];
+                result = '';   
+            }
+
+            sum = 0;
+        }
+
+        if (str[i] == 1) {
+            if (str[i + 1] == '1') {
+                result += '-';
+                isSpace = false
+            } else if (str[i + 1] == '0') {
+                result += '.';
+                isSpace = false
+            }
+        }
+
+        if (str[i] == '*') {
+            if (isSpace == false) {
+                value += ' ';
+                isSpace = true;
+            } 
+        }
+
+        sum += 2;
+    }
+
+    return value;
 }
 
 module.exports = {
